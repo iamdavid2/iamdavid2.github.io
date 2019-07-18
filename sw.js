0d3cb746a94c34e92ed216761a6582336c700dda
iamdavid2 <miemailesdavid@yahoo.com>
2019-07-18 14:56:55 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -15,13 +15,3 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    cache.match(event.request).then(function(response) {
-      return response || fetch(event.request).then(function(response) {
-        cache.put(event.request, response.clone());
-        return response;
-      });
-    });
-  );
-});
