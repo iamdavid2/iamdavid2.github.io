cf5c633a6c5e77a9960839ae6233654fcece0074
David Camilo Herrera Moreno <david.herrera@globant.com>
2019-07-18 10:19:49 -0500

    m

---
+++ sw.js
@@ -0,0 +1,27 @@
+---
+layout: null
+---
+// Cache name: adjust version number to invalidate service worker cachce.
+var CACHE_NAME = 'james-ives-cache-v2';
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    cache.match(event.request).then(function(response) {
+      return response || fetch(event.request).then(function(response) {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    });
+  );
+});
