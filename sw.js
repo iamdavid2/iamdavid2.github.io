413c0f7f4781805579b6cf9a5abb955b5b81870d
David Camilo Herrera Moreno <david.herrera@globant.com>
2019-07-18 19:08:51 -0500

    site

--- sw.js
+++ sw.js
@@ -20,3 +20,6 @@ self.addEventListener('install', function(event) {
   );
 });
 
+this.addEventListener('fetch', function (event) {
+  // it can be empty if you just want to get rid of that error
+});
